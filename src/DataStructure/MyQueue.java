c8b4bed713eee00dd03b041b04b71a6a8d636146
Joon <93jpark@gmail.com>
2021-11-02 12:02:06 +0900

    implement basic function of Queue

--- src/DataStructure/MyQueue.java
+++ src/DataStructure/MyQueue.java
@@ -1,2 +1,21 @@
-package DataStructure;public class MyQueue {
+package DataStructure;
+
+import java.util.*;
+
+public class MyQueue<T> {
+
+    private ArrayList<T> queue = new ArrayList<T>();
+
+    public boolean enqueue(T item) {
+        this.queue.add(item);
+        return true;
+    }
+
+    public T dequeue() {
+        if(this.queue.isEmpty()) {
+            return null;
+        } else {
+            return this.queue.remove(this.queue.size()-1);
+        }
+    }
 }
