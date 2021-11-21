5ac151b2527534af47c567c19b2ed6c78b1881e9
Joon <93jpark@gmail.com>
2021-11-21 22:28:33 +0900

    implements max heap constructor

--- src/DataStructure/MyMaxHeap.java
+++ src/DataStructure/MyMaxHeap.java
@@ -2,8 +2,18 @@ package DataStructure;
 
 public class MyMaxHeap {
 
+    public MyNode head;
+    public Integer size;
 
     public MyMaxHeap() {
-
+        this.head = null;
+        this.size = 0;
     }
+
+    public MyMaxHeap(Integer data) {
+        this.head = new MyNode(data);
+        this.size = 1;
+    }
+
+
 }
