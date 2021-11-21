7e1859a2cecd020f4194a25083f6e35d4d2cddb2
Joon <93jpark@gmail.com>
2021-11-21 10:13:19 +0900

    implements BST insert

--- src/DataStructure/MyBinarySearchTree.java
+++ src/DataStructure/MyBinarySearchTree.java
@@ -10,12 +10,26 @@ public class MyBinarySearchTree {
         this.size = 0;
     }
 
-    public boolean insert() {
-        // case1 : nothing stored
-        if(this.head == null) {
-
+    public boolean insert(Integer data) {
+        MyNode newNode = new MyNode(data);
+        MyNode target = this.head;
+        // case 1 : nothing stored
+        while (true) {
+            System.out.println("now checking .. " + target.value);
+            if (target == null) {
+                target = newNode;
+                size++;
+                break;
+            } else {
+                // case 2 : node value is greater than new data
+                if (target.value > data) {
+                    target = target.left;
+                    // case 3 : node value is less or equal with new data
+                } else {
+                    target = target.right;
+                }
+            }
         }
-
-        return true;
+        return false;
     }
 }
