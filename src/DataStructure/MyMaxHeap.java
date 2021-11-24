7da43d5c292876e7f39307f7dd0cebf0a1be2c17
Joon <93jpark@gmail.com>
2021-11-25 03:57:00 +0900

    implements extractMax() with others

--- src/DataStructure/MyMaxHeap.java
+++ src/DataStructure/MyMaxHeap.java
@@ -1,17 +1,8 @@
 package DataStructure;
 
-import com.sun.security.jgss.GSSUtil;
 
 import java.util.*;
 
-
-     /*
-                                example
-        current node: i        - 0, 3
-        parent: (i-1)/2        - 1, 1
-        left child: (i*2)+1    - 3, 7
-        right child: (i*2)+2   - 4, 8
-     */
 
 public class MyMaxHeap {
 
@@ -31,15 +22,123 @@ public class MyMaxHeap {
 
     
     public boolean insert(Integer data) {
-        int current = this.size;
-
+        int current = this.size; // 7
         this.heap.add(data);
-        this.heapify(current);
         this.size++;
+
+        // reversed heapify process
+        // from bottom to top
+        while (current != 0) {
+            if(this.heap.get(current) > this.heap.get(this.getParentPos(current))) {
+                swap(current, this.getParentPos(current));
+            }
+
+            current = this.getParentPos(current);
+        }
+
 
         return true;
     }
 
+
+    public Integer extractMax() {
+        if(this.size == 0) {
+            System.out.println("Heap is empty");
+            return null;
+        }
+        int max = this.heap.get(0);
+        int index = 0;
+        this.swap(0, this.size-1);
+        this.heap.remove(this.size-1);
+        this.size--;
+        this.heapify(0);
+
+        return max;
+
+    }
+
+    // heapify from top to bottom
+    public boolean heapify(int pos) {
+
+        if (this.isLeaf(pos)) {
+            return true;
+        }
+
+        // subtree에서 해당 Posisition을 기준으로 좌/우에 자식이 더 크다면,
+        if(this.heap.get(pos) < this.heap.get(this.getLeftPos(pos)) ||
+                this.heap.get(pos) < this.heap.get(this.getRightPos(pos)) ) {
+            // 왼쪽 자식이 더 크다면
+            if(this.heap.get(pos) < this.heap.get(this.getLeftPos(pos)) ) {
+                // 왼쪽 자식과 부모를 스왑한 후, 왼쪽 자식을 기준으로 다시 heapify
+                swap(pos,this.getLeftPos(pos));
+                heapify(this.getLeftPos(pos));
+            } else if (this.heap.get(pos) < this.heap.get(this.getRightPos(pos)) ) {
+                swap(pos, this.getRightPos(pos));
+                heapify(this.getRightPos(pos));
+            }
+        }
+
+        return true;
+    }
+
+    public boolean swap(int posA, int posB) {
+        if(posA > this.size || posB > this.size) {
+            if(posA > this.size) {
+                System.out.println(posA + " is out of range A");
+            }
+            if(posB > this.size) {
+                System.out.println(posB + " is out of range B");
+            }
+            System.out.println("Failed to swap");
+        }
+
+        int tmp = this.heap.get(posA);
+        this.heap.set(posA, this.heap.get(posB));
+        this.heap.set(posB, tmp);
+        return true;
+    }
+
+
+    public Integer getMax() {
+        if(this.size == 0) {
+            System.out.println("Heap is empty");
+            return null;
+        }
+        return this.heap.get(0);
+    }
+
+    public boolean isLeaf(int pos) {
+        if(this.getLeftPos(pos)>=this.size && this.getRightPos(pos) >= size) {
+            return true;
+        }
+        return false;
+    }
+        /*
+                                   example
+            current node: i        - 0, 3
+            parent: (i-1)/2        - 1, 1
+            left child: (i*2)+1    - 3, 7
+            right child: (i*2)+1   - 4, 8
+         */
+    public Integer getParentPos(int pos) {
+        return (pos-1)/2;
+    }
+
+    public Integer getLeftPos(int pos) {
+        return (pos*2)+1;
+    }
+
+    public Integer getRightPos(int pos) {
+        return (pos*2)+1;
+    }
+
+    public String toString() {
+        return this.heap.toString();
+    }
+
+    public void print() {
+        System.out.println(this.toString());
+    }
 
     // validate heap rule
     public boolean validate() {
@@ -51,74 +150,5 @@ public class MyMaxHeap {
             }
         }
         return true;
-    }
-
-    public boolean heapify(int pos) {
-        if(pos == 0) {
-            System.out.println("Heapify done");
-            return true;
-        }
-        /*
-        System.out.println("--------------------------");
-        System.out.println("current pos is "+ pos);
-        System.out.println("parent pos is "+ this.getParentPos(pos));
-        System.out.println("--------------------------");
-        */
-        int current = this.heap.get(pos);
-        int parent = this.heap.get(this.getParentPos(pos));
-        if(current > parent) {
-            swap(pos, this.getParentPos(pos));
-            heapify(this.getParentPos(pos));
-        }
-
-
-
-
-        return true;
-    }
-
-    public boolean swap(int posA, int posB) {
-
-        if(posA > this.size || posB > this.size) {
-            if(posA > this.size) {
-                System.out.println(posA + " is out of range");
-            }
-            if(posB > this.size) {
-                System.out.println(posB + " is out of range");
-            }
-            System.out.println("Failed to swap");
-        }
-
-        int tmp = this.heap.get(posA);
-        this.heap.set(posA, this.heap.get(posB));
-        this.heap.set(posB, tmp);
-
-        //System.out.println(posA+"'s "+this.heap.get(posA)+" is swapped with "+posB+"'s "+this.heap.get(posB));
-        return true;
-    }
-
-    public boolean isLeaf(int pos) {
-        int lpos = this.getLeftPos(pos);
-        int rpos = this.getRightPos(pos);
-        if(lpos > this.size || rpos > this.size) {
-            return true;
-        }
-        return false;
-    }
-
-    public Integer getParentPos(int pos) {
-        return (pos-1)/2;
-    }
-
-    public Integer getLeftPos(int pos) {
-        return (pos*2)+1;
-    }
-
-    public Integer getRightPos(int pos) {
-        return (pos*2)+2;
-    }
-
-    public String toString() {
-        return this.heap.toString();
     }
 }
