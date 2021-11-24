6f1629532f9860a16c20e2278a621dfb517acc6f
Joon <93jpark@gmail.com>
2021-11-24 16:14:05 +0900

    implements MaxHeap insert, validate, heapify

--- src/DataStructure/MyMaxHeap.java
+++ src/DataStructure/MyMaxHeap.java
@@ -4,32 +4,7 @@ import com.sun.security.jgss.GSSUtil;
 
 import java.util.*;
 
-public class MyMaxHeap {
 
-    public LinkedList<Integer> heap; // heap to store data
-    public Integer size; // current heap size
-
-    public MyMaxHeap() {
-        this.heap = null;
-        this.size = 0;
-    }
-
-    public MyMaxHeap(Integer data) {
-        this.heap.add(data);
-        this.size += 1;
-    }
-
-    
-    public boolean insert(Integer data) {
-        int current = this.size;
-
-        this.heap.add(data);
-        this.heapify();
-
-        this.size++;
-
-        return true;
-    }
      /*
                                 example
         current node: i        - 0, 3
@@ -38,8 +13,65 @@ public class MyMaxHeap {
         right child: (i*2)+2   - 4, 8
      */
 
+public class MyMaxHeap {
 
-    public boolean heapify() {
+    public LinkedList<Integer> heap; // heap to store data
+    public Integer size; // current heap size
+
+    public MyMaxHeap() {
+        this.heap = new LinkedList<>();
+        this.size = 0;
+    }
+
+    public MyMaxHeap(Integer data) {
+        this.heap = new LinkedList<>();
+        this.heap.add(data);
+        this.size = 1;
+    }
+
+    
+    public boolean insert(Integer data) {
+        int current = this.size;
+
+        this.heap.add(data);
+        this.heapify(current);
+        this.size++;
+
+        return true;
+    }
+
+
+    // validate heap rule
+    public boolean validate() {
+        for(int i = this.size-1; i >=0; i--) {
+            int parent = this.getParentPos(i);
+            if(this.heap.get(parent) < this.heap.get(i)) {
+                System.out.println("parent("+parent+" index)'s value "+this.heap.get(parent)+" is less against "+i+" index's "+this.heap.get(i));
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean heapify(int pos) {
+        if(pos == 0) {
+            System.out.println("Heapify done");
+            return true;
+        }
+        /*
+        System.out.println("--------------------------");
+        System.out.println("current pos is "+ pos);
+        System.out.println("parent pos is "+ this.getParentPos(pos));
+        System.out.println("--------------------------");
+        */
+        int current = this.heap.get(pos);
+        int parent = this.heap.get(this.getParentPos(pos));
+        if(current > parent) {
+            swap(pos, this.getParentPos(pos));
+            heapify(this.getParentPos(pos));
+        }
+
+
 
 
         return true;
@@ -61,17 +93,32 @@ public class MyMaxHeap {
         this.heap.set(posA, this.heap.get(posB));
         this.heap.set(posB, tmp);
 
-        System.out.println(posA+"'s "+this.heap.get(posA)+" is swapped with "+posB+"'s "+this.heap.get(posB));
+        //System.out.println(posA+"'s "+this.heap.get(posA)+" is swapped with "+posB+"'s "+this.heap.get(posB));
         return true;
     }
 
     public boolean isLeaf(int pos) {
-        int lpos = (pos*2) + 1;
-        int rpos = (pos*2) + 2;
+        int lpos = this.getLeftPos(pos);
+        int rpos = this.getRightPos(pos);
         if(lpos > this.size || rpos > this.size) {
             return true;
         }
         return false;
     }
 
+    public Integer getParentPos(int pos) {
+        return (pos-1)/2;
+    }
+
+    public Integer getLeftPos(int pos) {
+        return (pos*2)+1;
+    }
+
+    public Integer getRightPos(int pos) {
+        return (pos*2)+2;
+    }
+
+    public String toString() {
+        return this.heap.toString();
+    }
 }
