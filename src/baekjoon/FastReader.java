0a878cb53e5bd021b63c8d86a62e7b3e391baa5d
Joon <93jpark@gmail.com>
2021-11-28 14:11:41 +0900

    FastReader user defined class for IO processing

---
+++ src/baekjoon/FastReader.java
@@ -0,0 +1,54 @@
+package baekjoon;
+import java.util.*;
+import java.io.*;
+
+public class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    // constructor
+    public FastReader() {
+        // get input stream for System input
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    // constructor when get String as input
+    public FastReader(String s) throws FileNotFoundException {
+        br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    //
+    String next() {
+        while(st ==  null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextIne() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    // get string with exception handling
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
