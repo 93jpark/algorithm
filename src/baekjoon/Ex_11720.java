380c6383c4b4faa0a204e3e9144c5b39f94bc91f
Joon <93jpark@gmail.com>
2021-11-28 15:46:27 +0900

    changed into FastReader to get input

--- src/baekjoon/Ex_11720.java
+++ src/baekjoon/Ex_11720.java
@@ -5,10 +5,55 @@ import java.util.*;
 import java.io.*;
 
 public class Ex_11720 {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        // constructor
+        public FastReader() {
+            // get input stream for System input
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble() { return Double.parseDouble(next()); }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt();
-        BigInteger n = new BigInteger(sc.next());
+
+        FastReader scan = new FastReader();
+
+        int k = scan.nextInt();
+        BigInteger n = new BigInteger(scan.next());
         BigInteger sum = new BigInteger("0");
 
         for(int i = 0; i < k; i++) {
@@ -17,4 +62,5 @@ public class Ex_11720 {
         }
         System.out.println(sum.toString());
     }
+
 }
