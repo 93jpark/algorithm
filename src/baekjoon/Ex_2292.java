c49ce4b58b47e1223c1809837d6514fdf0e4bd48
Joon <93jpark@gmail.com>
2021-12-03 11:32:07 +0900

    baekjoon prolbem #2292 solved

--- src/baekjoon/Ex_2292.java
+++ src/baekjoon/Ex_2292.java
@@ -1,4 +1,63 @@
 package baekjoon;
 
+import java.util.*;
+import java.io.*;
+
 public class Ex_2292 {
+
+    static int n;
+
+    static void input() {
+        FastReader scan = new FastReader();
+        n = scan.nextInt();
+    }
+
+    public static void main(String[] args) {
+        input();
+        int increase = 0;
+        int t = 1;
+        while( n > t ) {
+            t = t + 6 * ++increase;
+        }
+        System.out.println(increase+1);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+
+    }
 }
