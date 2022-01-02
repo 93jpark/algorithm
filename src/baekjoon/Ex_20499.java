d76c158e8e611127ae765f6ca8c9beec8337871e
Joonwoo Park <93jpark@gmail.com>
2022-01-03 03:29:02 +0900

    baekjoon problem #20499 solved

---
+++ src/baekjoon/Ex_20499.java
@@ -0,0 +1,70 @@
+package baekjoon;
+
+import com.sun.jdi.IntegerType;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Ex_20499 {
+
+    static int[] kda = new int[3];
+
+    static void input() {
+        FastReader scan = new FastReader();
+        String s = scan.next();
+        for(int i = 0; i < 3; i++) {
+            kda[i] = Integer.parseInt(s.split("/")[i]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        input();
+        calc();
+    }
+
+    static void calc() {
+        String s = ((kda[0]+kda[2]) < kda[1]) || kda[1] == 0 ? "hasu" : "gosu";
+        System.out.println(s);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() {return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+    }
+}
