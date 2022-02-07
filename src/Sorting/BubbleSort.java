a219e6f4654ac33e8821cc2b2bc5a17c7c9e1407
Joonwoo Park <93jpark@gmail.com>
2022-02-07 22:16:49 +0900

    bubble sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -5,18 +5,32 @@ import java.util.*;
 public class BubbleSort {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(4);
-        list.add(2);
-        list.add(9);
+        list.add(3);
         list.add(7);
+        list.add(5);
+        list.add(1);
+        list.add(2);
+        list.add(8);
+        list.add(9);
+
+        System.out.println(list);
+
 
 
         for(int i=0; i<list.size()-1; i++) {
-            if(list.get(i) > list.get(i+1)) {
-                Collections.swap(list, i, i+1);
+            boolean flag = true;
+
+            for(int j=0; j<list.size()-1-i;j++){
+                if(list.get(j) > list.get(j+1)) {
+                    Collections.swap(list, j, j+1);
+                    flag = false;
+                    System.out.println(list);
+                }
+            }
+            if(flag) {
+                break;
             }
         }
 
-        System.out.println(list);
     }
 }
