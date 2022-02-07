9dfedfc6ea2b3ab395c0f05f8856fbabd724f685
Joonwoo Park <93jpark@gmail.com>
2022-02-07 22:19:33 +0900

    add comment

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -15,18 +15,18 @@ public class BubbleSort {
 
         System.out.println(list);
 
-
-
         for(int i=0; i<list.size()-1; i++) {
-            boolean flag = true;
+            boolean flag = true; // 교체된 이력을 체크하는 flag 변수
 
+            // j for문에서 -i를 하는 이유는 이미 정렬된 뒤쪽 원소들은 더이상 체크하지 않음
             for(int j=0; j<list.size()-1-i;j++){
                 if(list.get(j) > list.get(j+1)) {
                     Collections.swap(list, j, j+1);
-                    flag = false;
+                    flag = false; // 교체된 이력이 있음
                     System.out.println(list);
                 }
             }
+            // 한번도 swap이 일어난 적이 없다면 모든 원소가 정렬됨을 의미함
             if(flag) {
                 break;
             }
