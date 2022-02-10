27ada6d9b5eeadc808de14855bc6cf57e7d36593
Joonwoo Park <93jpark@gmail.com>
2022-02-10 10:10:56 +0900

    problem 10757 solved

---
+++ src/baekjoon/Ex_10757.java
@@ -0,0 +1,73 @@
+package baekjoon;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Ex_10757 {
+
+    static BigInteger A;
+    static BigInteger B;
+
+    static void input() {
+        FastReader scan = new FastReader();
+        A = new BigInteger(scan.next());
+        B = new BigInteger(scan.next());
+
+    }
+
+    public static void main(String[] args) {
+        input();
+        calc();
+    }
+
+    static void calc() {
+        System.out.println(A.add(B));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
