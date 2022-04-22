659b398e03f598bf630d26bd808cc0bbe21d6194
Joonwoo Park <93jpark@gmail.com>
2022-04-22 19:44:55 +0900

    Leetcode 2236 solved

--- src/leetcode/Ex_2236.java
+++ src/leetcode/Ex_2236.java
@@ -1,9 +1,14 @@
 package leetcode;
 
-import javax.swing.tree.TreeNode;
 
 public class Ex_2236 {
     public static boolean checkTree(TreeNode root) {
         return (root.left.val + root.right.val) == root.val;
     }
+}
+
+class TreeNode{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 }
