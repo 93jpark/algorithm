3bcc161414fd3e50296ec7656c843d1affbea617
Joonwoo Park <93jpark@gmail.com>
2022-05-02 16:32:06 +0900

    Programmers 86051 solved

---
+++ src/Programmers/Ex_86051.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+import java.util.*;
+
+public class Ex_86051 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n : numbers) {
+            set.add(n);
+        }
+
+        for(int i=0; i<10; i++) {
+            if(!set.contains(i)) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
