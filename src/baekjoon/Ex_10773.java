ea669ad8738618fb41d9684c999d76bcac6a8931
Joonwoo Park <93jpark@gmail.com>
2022-05-26 16:53:29 +0900

    Baekjoon 10773 제로 solved

--- src/baekjoon/Ex_10773.java
+++ src/baekjoon/Ex_10773.java
@@ -1,2 +1,39 @@
-package baekjoon;public class Ex_10773 {
+package baekjoon;
+
+/*
+    백준 스택
+    제로
+ */
+
+import java.util.*;
+
+public class Ex_10773 {
+
+    public static int T;
+    public static Stack<Integer> stack;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void input() {
+        T = sc.nextInt();
+        stack = new Stack<>();
+    }
+
+    public static void main(String[] args) {
+        input();
+        System.out.println(calc());
+    }
+
+    public static int calc() {
+
+        for(int i=0; i<T; i++) {
+            int num = sc.nextInt();
+            if(num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        return stack.stream().mapToInt(i -> i.intValue()).sum();
+    }
 }
