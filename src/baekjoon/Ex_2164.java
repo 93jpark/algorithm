56cd1cc87894c5c7cdacaece6a79d3c6320d8d27
Joonwoo Park <93jpark@gmail.com>
2022-05-29 17:09:11 +0900

    baekjoon 2164 카드2 solved

--- src/baekjoon/Ex_2164.java
+++ src/baekjoon/Ex_2164.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+/*
+    백준 덱/큐
+    2164 카드2
+ */
 public class Ex_2164 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
