2a1e36b6f13d62f9163e363d298ffbc1e4c7336c
Joonwoo Park <93jpark@gmail.com>
2022-06-25 13:24:52 +0900

    leetcode 113 solved

--- src/leetcode/Ex_113.java
+++ src/leetcode/Ex_113.java
@@ -2,24 +2,13 @@ package leetcode;
 import java.util.*;
 public class Ex_113 {
 
-    public static void main(String[] args) {
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        l1.add(1);
-        l1.add(2);
-        l1.add(3);
 
-        l2.addAll(l1);
-
-        l1.stream().mapToInt(a -> a).sum();
-
-        System.out.println(l1);
-        System.out.println(l2);
-    }
-    public static List<List<Integer>> lists = new ArrayList<>();
+    public static List<List<Integer>> lists;
+    public static int finalSum;
 
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-
+        lists = new ArrayList<>();
+        finalSum = targetSum;
 
 
         if(root == null) return lists;
@@ -33,23 +22,24 @@ public class Ex_113 {
     public static void recursive(TreeNode node, List<Integer> list) {
         if(node.left == null && node.right == null) {
             list.add(node.val);
-            lists.add(list);
+            if(finalSum == list.stream().mapToInt(a->a).sum()) {
+                lists.add(list);
+            }
         } else {
             list.add(node.val);
             if(node.left != null) {
                 List<Integer> newList = new ArrayList<>();
                 newList.addAll(list);
-                newList.add(node.left.val);
                 recursive(node.left, newList);
             }
             if(node.right != null) {
                 List<Integer> newList = new ArrayList<>();
                 newList.addAll(list);
-                newList.add(node.right.val);
                 recursive(node.right, newList);
             }
         }
     }
+
 
 
 }
